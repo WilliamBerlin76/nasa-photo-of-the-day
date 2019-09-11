b2f9952dcdcbb117889cd54425ca38fa666317f4
WilliamBerlin76 <william.berlin76@gmail.com>
2019-09-11 17:53:36 -0500

    title displayed

--- src/Components/photoCard.js
+++ src/Components/photoCard.js
@@ -3,6 +3,7 @@ import React from "react";
 const PhotoCard = props =>{
     return(
         <div className="card" key={props.id}>
+            <h3 className='photoTitle'>{props.title}</h3>
             <img className='spaceImage' alt="space image" src={props.imgUrl}></img>
             <p className='photoExplanation'>{props.explanation}</p>
         </div>
