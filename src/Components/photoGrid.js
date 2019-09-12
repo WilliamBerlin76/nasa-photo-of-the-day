21cfbe5c873fe997c078f50d7ac8127055e9b046
WilliamBerlin76 <william.berlin76@gmail.com>
2019-09-12 18:09:09 -0500

    styling the input section

--- src/Components/photoGrid.js
+++ src/Components/photoGrid.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import PhotoCard from "./photoCard";
 import axios from 'axios';
+import styled from "styled-components";
 
 export default function PhotoGrid () {
     const [photo, setPhoto] = useState([])
@@ -27,12 +28,37 @@ export default function PhotoGrid () {
         })
     }, [photoDate])
 
+    const SearchButton = styled.button`
+        margin-top: 10px;
+        color: #4B0082;
+        background: silver;
+        border: 0px solid silver;
+        border-radius: 20px;
+        width: 150px;
+        height: 30px;
+        font-weight: bold;
+        cursor: pointer;
+        transition: 0.2s ease-in;
+        &:hover {
+            background: #4B0082;
+            color: silver
+        }
+    `;
+  
+    const FillableForm = styled.form`
+        margin: 0 auto;
+        font-size: 18px;
+        display: flex;
+        flex-direction: column;
+        width: 150px;
+        align-items: center;
+    `;
     return(
         <div className ="container">
-             <form onSubmit={handleSubmit}>Enter Date:
-               <input type="text" value={value} onChange={handleChange} placeholder=""></input>
-               <button type="submit">Submit Date</button>
-            </form>
+             <FillableForm onSubmit={handleSubmit}>Enter Date: 
+               <input type="text" value={value} onChange={handleChange} placeholder="yyyy-mm-dd"></input>
+               <SearchButton type="submit">Submit Date</SearchButton>
+            </FillableForm>
             <PhotoCard key={photo} title={photo.title} imgUrl={photo.url} explanation={photo.explanation}/>
         </div>
     )
